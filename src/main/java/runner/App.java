4115caeb95f978ea3e45fa5d58c79ff661d66277
arkham <nits.slimshady@gmail.com>
2020-09-30 08:15:58 +0530

    updated to check if docker image picks up the changes

--- src/main/java/runner/App.java
+++ src/main/java/runner/App.java
@@ -2,6 +2,6 @@ package runner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(" HEY THERE! WE HAVE BUILT A PROJECT FROM DOCKER !");
+        System.out.println(" HEY THERE! WE HAVE BUILT A PROJECT FROM DOCKER ! VERSION2");
     }
 }
